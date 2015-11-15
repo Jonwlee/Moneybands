e7d6413d3fd412a5aa0c0169876eda603723490d
Jonathan Lee <Jonwlee457@gmail.com>
2015-11-15 10:43:54 -0500

    beautifier/ still broken

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,16 +1,16 @@
 //Globals
-var stepGoalV; 
+var stepGoalV;
 var heartRateGoalV;
 var donationAmountV;
 
-function submit(){
-	stepGoalV = document.getElementById("stepGoal").value;
-	console.log(stepGoalV);
-	heartRateGoalV = document.getElementById("heartRateGoal").value;
-	console.log(heartRateGoalV);
-	donationAmountV = document.getElementById("donationAmount").value;
-	
-	document.getElementById("finalDonation").innerHTML = donationAmountV;
+function submit() {
+    stepGoalV = document.getElementById("stepGoal").value;
+    console.log(stepGoalV);
+    heartRateGoalV = document.getElementById("heartRateGoal").value;
+    console.log(heartRateGoalV);
+    donationAmountV = document.getElementById("donationAmount").value;
+
+    document.getElementById("finalDonation").innerHTML = donationAmountV;
 
 }
 
@@ -20,8 +20,8 @@ var authData;
 var ref = new Firebase("https://run-or-fund.firebaseio.com");
 
 document.getElementById('googleSign').addEventListener('click', ref.authWithOAuthRedirect("google", function(error) {
-  if (error) 
-    console.log("Authentication Failed!", error);
-} //closes anon function
-)//closes ref method
- );//closes eventlistner
+            if (error)
+                console.log("Authentication Failed!", error);
+        } //closes anon function
+    ) //closes ref method
+); //closes eventlistner
