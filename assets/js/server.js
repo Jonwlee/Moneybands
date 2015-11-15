68cf7e08ba2fedf7f7af0c0930ad33d432f22d3b
Jonathan Lee <Jonwlee457@gmail.com>
2015-11-15 06:08:23 -0500

    added login function  need to test on server need to style button

--- assets/js/server.js
+++ assets/js/server.js
@@ -13,3 +13,22 @@ function submit(){
 	document.getElementById("finalDonation").innerHTML = donationAmountV;
 
 }
+
+
+var signer;
+	
+
+var ref = new Firebase("https://run-or-fund.firebaseio.com");
+
+document.addEventListener('DOMContentLoaded', //this is one big parameter
+    function handlers() {
+        document.getElementById('googleSign').addEventListener('click', ref.authWithOAuthPopup("google", function(error, authData) {
+ 		 if (error) {
+   			console.log("Login Failed!", error);
+  		} else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+})
+); //event handlers
+
+    });
